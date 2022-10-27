a777e8bcb647d6a598057bde5879bdf5971ced82
Alima Asanbaeva <alima.asanbaeva@gmail.com>
2022-10-27 15:28:12 -0400

    Blockchain Routes Created

--- backend/POA.py
+++ backend/POA.py
@@ -1,9 +1,9 @@
-
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa,padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_private_key,load_pem_public_key
 import json
+import base64
 
 #generate private key and public key
 def generate_keys():
@@ -21,33 +21,32 @@ def generate_keys():
 
 
 
-
 def encrypt_signature(block_dict,admin_public_key,voter_private_key):
 
     voter_private_key=load_pem_private_key(voter_private_key,password=None)
     admin_public_key=load_pem_public_key(admin_public_key,backend=None)
 
-
-    #convert block_dict to json foramt
+    #convert block_dict to json format
     info=json.dumps(block_dict,sort_keys=True).encode()
     
-
     #signature
     signature=voter_private_key.sign(info,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
     
     #encrypt for info
     encrypt_info=admin_public_key.encrypt(info,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
-
-   
-    encrypt_and_signature=(encrypt_info,signature)
+    # TESTTTTTT
+    b64_encrypt_info = base64.b64encode(encrypt_info)
+    b64_signature = base64.b64encode(signature)
+    b64_encrypt_info = b64_encrypt_info.decode("utf-8")
+    b64_signature = b64_signature.decode("utf-8")
+    encrypt_and_signature=(b64_encrypt_info,b64_signature)
     #encrypt_and_signature[0] is encrypt_info, encrypt_and_signature[1] is the signature
     return encrypt_and_signature
 
 
 
 # proof of authority, verify if data has been tampered with
-
-def poa(admin_private_key,voter_public_key,encrypt_and_signature):
+def verify_vote(admin_private_key,voter_public_key,encrypt_and_signature):
     #load keys
     admin_private_key=load_pem_private_key(admin_private_key,password=None)
     voter_public_key=load_pem_public_key(voter_public_key,backend=None)
@@ -56,6 +55,9 @@ def poa(admin_private_key,voter_public_key,encrypt_and_signature):
     signature=encrypt_and_signature[1]
     #decrypt for info
     info=admin_private_key.decrypt(encrypted_info,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+    print('Info', info)
+    decoded_info = info.decode()
+    print('Decoded Info', decoded_info)
     
     #verification
     try:
@@ -63,19 +65,4 @@ def poa(admin_private_key,voter_public_key,encrypt_and_signature):
         return True
     except:
         return False
-    
-
-#check block's validity
-def is_valid_chain(chain):
-    #first block's previous hash
-    pre_hash="0"
-    for block in chain:
-        #record current block hash as next block's previous hash
-        current_hash=block.hash
-
-        #check if previous_hash is modified
-        if pre_hash!=block.previous_hash:
-            return False
-        pre_hash=current_hash
-    return True
 
