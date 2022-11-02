bde563645b387cefd93270bf1135f3212b8166f9
Zhipeng-Yang69 <Zhipeng.Yang69@myhunter.cuny.edu>
2022-11-02 12:59:38 -0400

    RSA combine with AES

--- backend/POA.py
+++ backend/POA.py
@@ -5,22 +5,20 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_private_key,load_pem_public_key
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-import os
 import json
 
 
 #generate 16bytes key for AES encrypt and decrypt
 
 def generate_16bytes_key():
-    key = get_random_bytes(16)
+    key = get_random_bytes(AES.block_size)
     return key
 
 #generate private key and public key
 def generate_keys():
    
    
-    private_key = rsa.generate_private_key(public_exponent=65537,key_size=4096,)
+    private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048,)
     public_key = private_key.public_key()
 
     
@@ -49,35 +47,36 @@ def encrypt_signature(block_dict,AES_key,admin_public_key,voter_private_key):
     #creat a AES cipher object
     cipher_encrypt=AES.new(AES_key,AES.MODE_CFB)
     ciphered_data = cipher_encrypt.encrypt(info)
+    
     iv = cipher_encrypt.iv
     #encrypt for info
-    encrypt_info=admin_public_key.encrypt(ciphered_data,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+    encrypt_aes_key=admin_public_key.encrypt(AES_key,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
 
    
-    encrypt_and_signature=(encrypt_info,signature,iv)
-    #encrypt_and_signature[0] is encrypt_info, encrypt_and_signature[1] is the signature
+    encrypt_and_signature=(encrypt_aes_key,signature,ciphered_data,iv)
+    #encrypt_and_signature[0] is encrypt_aes_key, encrypt_and_signature[1] is the signature,encrypt_and_signature[2] is encrypted info by AES, and iv is initial block for plaintext
     return encrypt_and_signature
 
 
 
 # proof of authority, verify if data has been tampered with
-def poa(admin_private_key,AES_key,voter_public_key,encrypt_and_signature):
+def poa(admin_private_key,voter_public_key,encrypt_and_signature):
     #load keys
     admin_private_key=load_pem_private_key(admin_private_key,password=None)
     voter_public_key=load_pem_public_key(voter_public_key,backend=None)
     
-    encrypted_info=encrypt_and_signature[0]
+    encrypted_aes_key=encrypt_and_signature[0]
     signature=encrypt_and_signature[1]
-    iv=encrypt_and_signature[2]
+    #iv=encrypt_and_signature[2]
     #RSA decrypt 
-    info=admin_private_key.decrypt(encrypted_info,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+    aes_key=admin_private_key.decrypt(encrypted_aes_key,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
     #AES decrypt
-    cipher_decrypt = AES.new(AES_key,AES.MODE_CFB, iv=iv)
-    deciphered_info = cipher_decrypt.decrypt(info)
+    cipher_decrypt = AES.new(aes_key,AES.MODE_CFB,encrypt_and_signature[3])
+    decrypt_info = cipher_decrypt.decrypt(encrypt_and_signature[2])
 
     #verification
     try:
-        voter_public_key.verify(signature,deciphered_info,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
+        voter_public_key.verify(signature,decrypt_info,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
         return True
     except:
         return False
