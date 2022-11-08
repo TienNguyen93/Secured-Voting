13e3f5bb3add39ef6c90543cd13565b8f70233e8
dive0 <zhengshiwei5@gmail.com>
2022-11-08 14:44:57 -0500

    Create get and put method in signup page

--- frontend/src/components/containers/SignupContainer.js
+++ frontend/src/components/containers/SignupContainer.js
@@ -1,11 +1,84 @@
 import { SignupView } from "../views";
-import React, { Component }  from 'react';
+import React, { Component, useState } from "react";
 
 class SignupContainer extends Component {
+    // Initialize state
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name: "",
+            last_name: "",
+            email: "",
+            date_of_birth: "",
+            ssn: "",
+            password: "",
+            confirm_password: "",
+        };
+    }
+
+    // Capture input data when it is entered
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault(); // Prevent browser reload/refresh after submit.
+
+        // store data from backend
+        const [voter, setVoter] = useState([]);
+
+        // fetch data from backend
+        // not working yet
+        fetch("/Voters").then((response) =>
+            response.json().then((data) => {
+                console.log(data); // testing data - remove after testing
+                setVoter(data);
+            })
+        );
+
+        const {
+            first_name,
+            last_name,
+            email,
+            date_of_birth,
+            ssn,
+            password,
+            confirm_password,
+        } = this.state;
+
+        // update data from backend
+        voter.map((voter) => {
+            if (voter.ssn === ssn && voter.voted === false) {
+                fetch("/Voters", {
+                    method: "PUT",
+                    headers: {
+                        "content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        first_name,
+                        last_name,
+                        email,
+                        date_of_birth,
+                        ssn,
+                        password,
+                        registered: true,
+                    }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data);
+                    });
+            }
+        });
+    };
+
     render() {
         return (
-            <SignupView />
-        )
+            <SignupView
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+            />
+        );
     }
 }
 
