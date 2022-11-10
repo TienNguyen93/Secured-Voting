92f5c96f3215bcb463ce7a929b62acab62e5c10b
dive0 <zhengshiwei5@gmail.com>
2022-11-10 01:06:32 -0500

    Create route to admin dashboard

--- frontend/src/components/containers/index.js
+++ frontend/src/components/containers/index.js
@@ -2,3 +2,4 @@ export { default as SignupContainer } from './SignupContainer';
 export { default as SigninContainer } from './SigninContainer';
 export { default as VotingContainer } from './VotingContainer';
 export { default as VoteSuccessContainer } from './VoteSuccessContainer';
+export { default as AdminContainer } from './AdminContainer';
