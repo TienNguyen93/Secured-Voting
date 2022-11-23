65c98690c9c955f2dc7ac82d72058c6a13ee6b7e
dive0 <zhengshiwei5@gmail.com>
2022-11-23 15:16:30 -0500

    Iterate through database for candidates

--- frontend/src/components/containers/VotingContainer.js
+++ frontend/src/components/containers/VotingContainer.js
@@ -1,12 +1,28 @@
-import { VotingView } from '../views';
-import React, { Component }  from 'react';
+import { VotingView } from "../views";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-class VotingContainer extends Component {
-    render() {
-        return (
-            <VotingView />
-        )
+const VotingContainer = () => {
+    const navigate = useNavigate();
+    // Get candidates from database
+    const [candidates, setCandidates] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/candidates").then((response) =>
+            response.json().then((data) => {
+                setCandidates(data);
+            })
+        );
+    }, []);
+
+    const handleSubmit = (e) => {
+
+        navigate("/vote-success");
     }
+
+    return (
+        <VotingView handleSubmit={handleSubmit} candidates={candidates}/>
+    );
 }
 
 export default VotingContainer;
--- frontend/src/components/views/VotingView.js
+++ frontend/src/components/views/VotingView.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React from "react";
 
-const VotingView = () => {
+const VotingView = (props) => {
+    const { handleSubmit, candidates } = props;
+
     return (
         <div>
             <h1>Vote</h1>
             <div>
                 <h2>President</h2>
-                <form>
-                    <input type="radio" id="president1" name="president" value="president1" />
-                    <label htmlFor="president1">President 1</label><br />
-                    <input type="radio" id="president2" name="president" value="president2" />
-                    <label htmlFor="president2">President 2</label><br />
-                    <input type="radio" id="president3" name="president" value="president3" />
-                    <label htmlFor="president3">President 3</label><br />
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    {/* {candidates.map(candidate => {
+                        return (
+                            <div>
+                                <input type="radio" name={candidate} />
+                                <label>{candidate}</label>
+                            </div>
+                        )
+                    })} */}
+
+                    <input
+                        type="radio"
+                        id="president1"
+                        name="president"
+                        value="president1"
+                    />
+                    <label htmlFor="president1">President 1</label>
+                    <br />
+                    <input
+                        type="radio"
+                        id="president2"
+                        name="president"
+                        value="president2"
+                    />
+                    <label htmlFor="president2">President 2</label>
+                    <br />
+                    <input
+                        type="radio"
+                        id="president3"
+                        name="president"
+                        value="president3"
+                    />
+                    <label htmlFor="president3">President 3</label>
+                    <br />
+
+                    <div className="button-container">
+                        <button>Submit</button>
+                    </div>
                 </form>
             </div>
-
-            <br />
-            <button>Submit</button>
         </div>
-    )
-}
+    );
+};
 
 export default VotingView;
