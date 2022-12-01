c1b68c18452b841cdbcad90a044d5a32fbca65b9
dive0 <zhengshiwei5@gmail.com>
2022-12-01 00:13:05 -0500

    Create post method for voters

--- frontend/src/components/containers/VoterContainer.js
+++ frontend/src/components/containers/VoterContainer.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 const VoterContainer = () => {
     const [voters, setVoters] = useState([]);
+    const [newVoter, setNewVoter] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:5000/voters")
@@ -13,8 +14,44 @@ const VoterContainer = () => {
         );
     }, [voters]);
 
+    const handleChange = (e) => {
+        let new_voter = {};
+        new_voter = { [e.target.name]: e.target.value };
+        setNewVoter((newVoter) => ({
+            ...newVoter,
+            ...new_voter,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // newVoter.dob = newVoter.dob.substring(5, 7) + "/" + newVoter.dob.substring(8) + "/" + newVoter.dob.substring(0, 4);
+
+        console.log(parseInt(newVoter.ssn));
+
+        const request = {
+            method: "POST",
+            headers: { "content-Type": "application/json" },
+            body: JSON.stringify({
+                firstname: newVoter.firstname,
+                lastname: newVoter.lastname,
+                email: newVoter.email,
+                dob: newVoter.dob,
+                ssn: parseInt(newVoter.ssn),
+            }),
+        }
+        fetch(`http://localhost:5000/voters`, request)
+            .then((response) => response.json())
+            .then((data) => console.log("post method", data))
+            .catch((error) => console.log(error))
+    };
+
     return (
-        <VoterView voters={voters}/>
+        <VoterView
+            handleChange={handleChange}
+            handleSubmit={handleSubmit} 
+            voters={voters}/>
     );
 };
 
--- frontend/src/components/views/adminComponents/VoterView.js
+++ frontend/src/components/views/adminComponents/VoterView.js
@@ -1,48 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./../navbar/Navbar";
 import "./Voter.css";
+import CandidateFormPopup from "./CandidateFormPopup";
 
 const VoterView = (props) => {
-	const { voters } = props;
+    const { handleChange, handleSubmit, voters } = props;
+    const [popupButton, setPopupButton] = useState(false);
 
     return (
         <div>
             <Navbar />
             <div className="VoterFormContainer">
                 <h1>Voters</h1>
-                <button>New</button>
+                <button onClick={() => setPopupButton(true)}>New</button>
                 <button>Remove</button>
                 <button>Edit</button>
+
+                <CandidateFormPopup
+                    trigger={popupButton}
+                    setTrigger={setPopupButton}
+                >
+                    <form
+                        onSubmit={(e) => {
+                            handleSubmit(e);
+                            setPopupButton(false);
+                        }}
+                    >
+                        <h1>Add Voter</h1>
+                        <label>First Name:</label>
+                        <input
+                            type="text"
+                            name="firstname"
+                            required
+                            onChange={(e) => handleChange(e)}
+                        />
+						<br />
+
+                        <label>Last Name:</label>
+                        <input
+                            type="text"
+                            name="lastname"
+                            required
+                            onChange={(e) => handleChange(e)}
+                        />
+						<br />
+
+                        <label>Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            onChange={(e) => handleChange(e)}
+                        />
+						<br />
+
+                        <label>Date of Birth:</label>
+                        <input
+                            type="date"
+                            name="dob"
+                            required
+                            onChange={(e) => handleChange(e)}
+                        />
+						<br />
+
+                        <label>Social Security Number:</label>
+                        <input
+                            type="text"
+                            name="ssn"
+                            required
+                            maxLength={9}
+                            onKeyPress={(event) => {
+                                if (!/[0-9]/.test(event.key)) {
+                                    event.preventDefault();
+                                }
+                            }}
+                            onChange={(e) => handleChange(e)}
+                        />
+						<br />
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </CandidateFormPopup>
 
                 <table className="table">
                     <thead>
                         <tr>
                             <th></th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email</th>
-							<th>Date of Birth</th>
-							<th>Account Registered</th>
-							<th>Voted</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Date of Birth</th>
+                            <th>Account Registered</th>
+                            <th>Voted</th>
                         </tr>
                     </thead>
-					<tbody>
-						{voters.map((voter) => {
-							return (
-								<tr>
-									<td>
-										<input type="radio" name="id" value={voter._id} />
-									</td>
-									<td>{voter.firstname}</td>
-									<td>{voter.lastname}</td>
-									<td>{voter.email}</td>
-									<td>{voter.dob}</td>
-									<td>{(voter.registered) ? "Yes" : "No"}</td>
-									<td>{(voter.voted) ? "Yes" : "No"}</td>
-								</tr>
-							);
-						})}
-					</tbody>
+                    <tbody>
+                        {voters.map((voter) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="id"
+                                            value={voter._id}
+                                        />
+                                    </td>
+                                    <td>{voter.firstname}</td>
+                                    <td>{voter.lastname}</td>
+                                    <td>{voter.email}</td>
+                                    <td>{voter.dob}</td>
+                                    <td>{voter.registered ? "Yes" : "No"}</td>
+                                    <td>{voter.voted ? "Yes" : "No"}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
                 </table>
             </div>
         </div>
