a9f32c5762567269a6f295722b16d3d88247d00a
TienNguyen93 <nguyentien9933@gmail.com>
2022-12-07 11:08:30 -0500

    retrieve chain, fix block styling, allow updates on voter's voted status

--- frontend/src/components/views/AdminView.js
+++ frontend/src/components/views/AdminView.js
@@ -4,69 +4,55 @@ import Navbar from "./navbar/Navbar";
 import "./adminComponents/Admin.css";
 import PieChart from "../containers/PieChartContainer";
 
-const AdminView = (props) => {
-    const { chain } = props;
+const AdminView = ({ chain }) => {
     const chainArray = chain.chain;
-
-    const handleStart = () => {
-        const configuration = {
-            method: 'post',
-            url: 'http://localhost:5000/init',
-        }
-        axios(configuration)
-            .then(result => {
-                console.log('res here', result)
-            }) 
-            .catch(error => {
-                console.log(error)
-            })
-    }
+    console.log('admin view chain', "& type is", typeof chainArray, chainArray)
 
     return (
         <div>
             <Navbar />
             <div className="AdminContainer">
                 <h1>Admin Dashboard</h1>
-                <button onClick={handleStart}>Start Election</button>
                 <PieChart />
 
                 <h2>Blockchain</h2>
                 <div className="blockchain-table">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Block</th>
-                            <th>Voter ID</th>
-                            <th>Voted Candidate</th>
-                            <th>Timestamp</th>
-                            <th>Previous Hash</th>
-                            <th>Hash</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(chainArray !== undefined) ?
-                            chainArray.map((block, index) => {
-                                if (index !== 0) {
-                                    return (
-                                        <tr>
-                                            <td>{block.index}</td>
-                                            <td>{block.voter}</td>
-                                            <td>{block.candidate}</td>
-                                            <td>{block.timestamp}</td>
-
-                                            <td className="a">{block.prev_hash}</td>
-                                            <td>{block.hash}</td>
-
-                                            <td className="a">{block.prev_hash.slice(0,10) + "..."}</td>
-                                            <td>{block.hash.slice(0,10) + "..."}</td>
-
-                                        </tr>
-                                    );
-                                }
-                            }) : console.log("failed")
-                        }
-                    </tbody>
-                </table>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Block</th>
+                                <th>Voter ID</th>
+                                <th>Voted Candidate</th>
+                                <th>Timestamp</th>
+                                <th>Previous Hash</th>
+                                <th>Hash</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {chainArray.map((block, i) => {
+                                return [
+                                    <tr key={i}>
+                                        <td>{block.index}</td>
+                                        <td>{block.voter}</td>
+                                        <td>{block.candidate}</td>
+                                        <td>{block.timestamp}</td>
+                                        <td className="hash">
+                                            {parseInt(block.prev_hash) === 0 
+                                            ? block.prev_hash
+                                            : block.prev_hash.slice(0, 10) + "..."
+                                            }
+                                            <span className="tooltip">{block.prev_hash}</span>
+                                        </td>
+                                        
+                                        <td className="hash">
+                                            {block.hash.slice(0, 10) + "..."}
+                                            <span className="tooltip">{block.hash}</span>
+                                        </td>
+                                    </tr>
+                                ]
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
